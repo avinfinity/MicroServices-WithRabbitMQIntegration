7a2aac4e202904134da812c520af83e8cdd4fec5
Avinash Rai <avinfinity@gmail.com>
2020-04-13 12:44:30 +0530

    Updated the randomly failing test cases due to existing databases tables after first unit test run

--- IntegrationUnitTests/IntegrationTests.cs
+++ IntegrationUnitTests/IntegrationTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
 using Pricing.API;
 using Pricing.Domain;
@@ -6,6 +7,7 @@ using ProductCatalogue.API.Commands;
 using ProductCatalogue.API.Queries;
 using ProductCatalogue.Domain;
 using ProductCatalogue.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,27 +17,54 @@ using Xunit;
 
 namespace IntegrationUnitTests
 {
-    public class IntegrationTests
+    public class Servers : IDisposable
     {
+        public Servers()
+        {
+            ProductCatalogAPIServer = ServerSetups.CreateServer<ProductsDbContext, ProductCatalogue.API.Startup, ProductDTO, Product>("TestData//ProductsTestData.json",true);
+            PricingAPIServer = ServerSetups.CreateServer<ProductPriceDbContext, Pricing.API.Startup, ProductPriceDTO, ProductPrice>("TestData//PricesTestData.json");
+        }
+
+        public TestServer ProductCatalogAPIServer { get; private set; }
+
+        public TestServer PricingAPIServer { get; private set; }
+
+        public void Dispose()
+        {
+            //Delete database after unit tests
+            var dbContext = ProductCatalogAPIServer.Services.GetService(typeof(ProductsDbContext)) as ProductsDbContext;
+            dbContext.Database.EnsureDeleted();
+
+            ProductCatalogAPIServer.Dispose();
+            PricingAPIServer.Dispose();
+        }
+    }
+
+    public class IntegrationTests : IClassFixture<Servers>
+    {
+        private readonly Servers _servers;
+
+        public IntegrationTests(Servers server)
+        {
+            _servers = server;
+        }
+
         [Fact]
         public async void Test_GetAllProductsFromCatalogue()
         {
-            using (var server = ServerSetups.CreateServer<ProductsDbContext, ProductCatalogue.API.Startup, ProductDTO, Product>("TestData//ProductsTestData.json"))
+            using (var client = _servers.ProductCatalogAPIServer.CreateClient())
             {
-                using (var client = server.CreateClient())
+                var response = await client.GetAsync("api/v1/ProductCatalogue/");
+                var items = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(items);
+
+                var testData = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(File.ReadAllText("TestData//ProductsTestData.json"));
+
+                Assert.True(products.Count() >= testData.Count());
+
+                for (int i = 0; i < products.Count(); i++)
                 {
-                    var response = await client.GetAsync("api/v1/ProductCatalogue/");
-                    var items = await response.Content.ReadAsStringAsync();
-                    var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(items);
-
-                    var testData = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(File.ReadAllText("TestData//ProductsTestData.json"));
-
-                    Assert.Equal(products.Count(), testData.Count());
-
-                    for (int i = 0; i < products.Count(); i++)
-                    {
-                        Assert.True(products.ElementAt(i).productid == testData.ElementAt(i).productid);
-                    }
+                    Assert.True(products.ElementAt(i).productid == testData.ElementAt(i).productid);
                 }
             }
         }
@@ -46,31 +75,31 @@ namespace IntegrationUnitTests
             //Arrange
             //Both below servers and clients DO NOT talk to each other directly or via REST.
             //All communication is via EventBus - RabbitMQ over AMQP protocol
-            var productServer = ServerSetups.CreateServer<ProductsDbContext, ProductCatalogue.API.Startup, ProductDTO, Product>("TestData//ProductsTestData.json", true);
-            var priceServer = ServerSetups.CreateServer<ProductPriceDbContext, Pricing.API.Startup, ProductPriceDTO, ProductPrice>("TestData//PricesTestData.json");
+            using (var productClient = _servers.ProductCatalogAPIServer.CreateClient())
+            {
+                using (var priceClient = _servers.PricingAPIServer.CreateClient())
+                {
+                    //Act
+                    var addNewProductCommand = new AddNewProductCommand(10, "Addedproduct", "", "", 100, new decimal(12.25), "");
+                    var content = new StringContent(JsonConvert.SerializeObject(addNewProductCommand), Encoding.UTF8, "application/json");
 
-            var productClient = productServer.CreateClient();
-            var priceClient = priceServer.CreateClient();
+                    //1. Add a new product to product catalog microservice
+                    var productResponse = await productClient.PostAsync("api/v1/ProductCatalogue/new", content);
+                    //2. Query for price from pricing microservice for added product
+                    var priceResponse = await priceClient.GetAsync("api/v1/ProductPrice/" + addNewProductCommand.ProductId.ToString("D"));
 
-            //Act
-            var addNewProductCommand = new AddNewProductCommand(10, "Addedproduct", "", "", 100, new decimal(12.25), "");
-            var content = new StringContent(JsonConvert.SerializeObject(addNewProductCommand), Encoding.UTF8, "application/json");
+                    //Assert
+                    Assert.True(productResponse.StatusCode == System.Net.HttpStatusCode.OK);
 
-            //1. Add a new product to product catalog microservice
-            var productResponse = await productClient.PostAsync("api/v1/ProductCatalogue/new", content);
-            //2. Query for price from pricing microservice for added product
-            var priceResponse = await priceClient.GetAsync("api/v1/ProductPrice/" + addNewProductCommand.ProductId.ToString("D"));
-            
-            //Assert
-            Assert.True(productResponse.StatusCode == System.Net.HttpStatusCode.OK);
+                    var items = await priceResponse.Content.ReadAsStringAsync();
+                    var price = JsonConvert.DeserializeObject<ProductPrice>(items);
 
-            var items = await priceResponse.Content.ReadAsStringAsync();
-            var price = JsonConvert.DeserializeObject<ProductPrice>(items);
-
-            //ProductId is equal, which shows that after adding to ProductCatalog microservice, 
-            //this product is also avaliable within Pricing microservices over integration events via RabbitMQ Event Bus
-            Assert.True(price.ProductId == addNewProductCommand.ProductId);
-            Assert.True((price.Price - addNewProductCommand.UnitPrice) < new decimal(0.0000000000001)); //Prices are equal
+                    //ProductId is equal, which shows that after adding to ProductCatalog microservice, 
+                    //this product is also avaliable within Pricing microservices over integration events via RabbitMQ Event Bus
+                    Assert.True(price.ProductId == addNewProductCommand.ProductId);
+                    Assert.True((price.Price - addNewProductCommand.UnitPrice) < new decimal(0.0000000000001)); //Prices are equal
+                }
+            }
         }
     }
 }
